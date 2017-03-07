3ea351f8cd546675bd214441f9b54437d2bb6179
Pavlo Dragobetskyi <Pavlo.Fragobetskyi2@bwin.org>
2017-03-07 21:16:31 +0200

    PD added screenshoting realization and interface

--- thumbnailExtractor/Program.cs
+++ thumbnailExtractor/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace thumbnailExtractor
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Image file resize to thumbnail dimensions.
+        /// </summary>
+        /// <param name="path">Input image path.</param>
+        /// <param name="outputPath">The output image path.</param>
+        public void ImageFileResize(string path, string outputPath)
+        {
+            Image image = Image.FromFile(path);
+            Image result = image.GetThumbnailImage(400, 400, null, IntPtr.Zero);
+            result.Save(outputPath);
+        }
+
+
+        [STAThread]
         static void Main(string[] args)
         {
         }
